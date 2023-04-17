de3062948a3ea76fdbc53028a00ef58f04ead601
atul chougale <atulchougale96@gmail.com>
2023-04-17 15:36:41 +0530

    url

--- src/components/calculator/GoldCalculator.js
+++ src/components/calculator/GoldCalculator.js
@@ -7,6 +7,7 @@ import 'react-datepicker/dist/react-datepicker.css';
 import Select from 'react-select';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './GoldCalculator.css';
+axios.defaults.baseURL = process.env.REACT_APP_SERVER_DOMAIN;
 
 
 
@@ -32,7 +33,7 @@ function GoldCalculator() {
 
     const handleClick = async () => {
         try {
-            const { data } = await axios.get(`http://localhost:5000/gold/${currency}`);
+            const { data } = await axios.get(`/${currency}`);
 
             setId(data.goldRate._id)
             
@@ -50,7 +51,7 @@ function GoldCalculator() {
 
     const calculateGoldPrice = async () => {
         try {
-            const { data } = await axios.post(`http://localhost:5000/gold/rate/calculate/${id}`, { weight, karat });
+            const { data } = await axios.post(`/rate/calculate/${id}`, { weight, karat });
             setGoldData(data.goldData);
         } catch (error) {
             console.log(error)
@@ -59,7 +60,7 @@ function GoldCalculator() {
 
     const deleteData = async () => {
         try {
-            const response = await axios.delete(`http://localhost:5000/gold/${id}`);
+            const response = await axios.delete(`/${id}`);
             console.log(response.data.message);
             navigate('/')
             // Handle success response
